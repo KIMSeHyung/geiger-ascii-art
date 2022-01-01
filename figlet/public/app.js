162492d76bd0789faef70910c682940c033ebb9c
KIM Se Hyung <tpgud33@gmail.com>
2022-01-02 04:46:32 +0900

    로고변경

--- figlet/public/app.js
+++ figlet/public/app.js
@@ -1,9 +1,4 @@
-const geiger = `  
- ____ _____ ___ ____ _____ ____
-/ ___| ____|_  _/ ___| ____|  _ \\
-| |  _|  _|  | | |  _|  _| | |_) |
-| |_| | |___ | | |_| | |___|  _ <
- \\____|_____|___\\____|_____|_| \\_\\`;
+const geiger = ``;
 
 mdc.textField.MDCTextField.attachTo(document.querySelector(".mdc-text-field"));
 mdc.ripple.MDCRipple.attachTo(document.querySelector(".mdc-button"));
@@ -12,6 +7,17 @@ const ascii = document.getElementById("ascii");
 ascii.innerText = geiger;
 console.log(geiger);
 console.log("-------ASCII ART FOR GEIGER-------");
+
+fetch(`/geiger/${text}`)
+.then((res) => res.text())
+.then((data) => {
+  console.log(data);
+  ascii.innerText = data;
+})
+.catch((error) => {
+  console.log(error);
+  alert("에러발생");
+});
 
 const submitBtn = document.querySelector("#submit");
 const input = document.querySelector(".mdc-text-field__input");
